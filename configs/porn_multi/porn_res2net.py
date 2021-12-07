cc4456628c8c78e1a0fa1f4dd87b3b05260beb02
dragonchemist <785524367@qq.com>
2021-12-07 21:24:20 +0800

    x

--- configs/porn_multi/porn_res2net.py
+++ configs/porn_multi/porn_res2net.py
@@ -135,7 +135,7 @@ data = dict(
         type=dataset_type,
         data_prefix='',
         classes=['性感_胸部', '色情_女胸', '色情_男下体', '色情_口交', '性感_内衣裤', '性感_男性胸部', '色情_裸露下体', '性感_腿部特写'],
-        ann_file='/home/zhou/projects/mmclassification/data/porn/porn_with_patch/train_p2.txt',
+        ann_file='/home/zhou/projects/mmclassification/data/porn/porn_with_patch/train.v3.txt',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
@@ -149,8 +149,9 @@ data = dict(
         classes=['性感_胸部', '色情_女胸', '色情_男下体', '色情_口交', '性感_内衣裤', '性感_男性胸部', '色情_裸露下体', '性感_腿部特写'],
         ann_file='/home/zhou/projects/mmclassification/data/porn/porn_with_patch/eval.txt',
         pipeline=test_pipeline))
-load_from = 'https://download.openmmlab.com/mmclassification/v0/regnet/convert/RegNetX-4.0GF-ef8bb32c.pth'
-evaluation = dict(interval=5, metric=['mAP', 'CP', 'CR', 'CF1', 'OP', 'OR', 'OF1'])
+load_from = 'https://download.openmmlab.com/mmclassification/v0/res2net/res2net50-w26-s8_3rdparty_8xb32_in1k_20210927-f547a94b.pth'
+evaluation = dict(interval=5, metric=['mAP', 'CP', 'CR', 'CF1', 'OP', 'OR', 'OF1'],
+                  labels=['性感_胸部', '色情_女胸', '色情_男下体', '色情_口交', '性感_内衣裤', '性感_男性胸部', '色情_裸露下体', '性感_腿部特写'])
 
 # optimizer
 optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
