c9aee65e93aff261d80b90be28669c440505d593
dragonchemist <785524367@qq.com>
2021-12-07 21:10:35 +0800

    x

--- configs/porn_multi/porn_swin_transformer.py
+++ configs/porn_multi/porn_swin_transformer.py
@@ -1,4 +1,8 @@
 _base_ = [
+
+    '../_base_/models/swin_transformer/small_224.py',
+    '../_base_/datasets/imagenet_bs64_swin_224.py',
+    '../_base_/schedules/imagenet_bs1024_adamw_swin.py',
     '../_base_/default_runtime.py'
 ]
 fp16 = dict(loss_scale='dynamic')
