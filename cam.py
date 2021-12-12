ef9f24da1dbdf1e3e9957997f5f657edf5de087b
Wuxialang zhou <you@example.com>
2021-12-13 01:06:56 +0800

    x

--- cam.py
+++ cam.py
@@ -221,7 +221,7 @@ class Saver(Thread):
 
         x = 0
         y = 0
-        w = shape[1]
+        w = shape[0]
         h = shape[1]
         neighbor = min(shape[1], shape[0]) // 40
         fh, fw = frame.shape[0], frame.shape[1]
