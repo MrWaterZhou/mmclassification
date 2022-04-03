7c85d1033f607381fcb40312f2fd0223f81cbc38
Wuxialang zhou <you@example.com>
2022-04-04 00:17:15 +0800

    x

--- configs/porn_multi/porn_20220403.py
+++ configs/porn_multi/porn_20220403.py
@@ -163,18 +163,14 @@ evaluation = dict(interval=5, metric=['mAP', 'CP', 'CR', 'CF1', 'OP', 'OR', 'OF1
                   labels=['性感_胸部', '色情_女胸', '色情_男下体', '色情_口交', '性感_内衣裤', '性感_男性胸部', '色情_裸露下体', '性感_腿部特写', '正常'])
 
 # optimizer
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.4, momentum=0.9, weight_decay=5e-5)
 optimizer_config = dict(grad_clip=None)
+# learning policy
 lr_config = dict(
     policy='CosineAnnealing',
     min_lr=0,
     warmup='linear',
-    warmup_iters=390*3,
-    warmup_ratio=0.01)
-
-# learning policy
-custom_hooks = [
-    dict(type='EMAHook', interval=100, priority='HIGH')
-]
-
-runner = dict(type='EpochBasedRunner', max_epochs=200)
+    warmup_iters=5,
+    warmup_ratio=0.1,
+    warmup_by_epoch=True)
+runner = dict(type='EpochBasedRunner', max_epochs=100)
