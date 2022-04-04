64850042cad34e87da5ce62f085858b64d5e858c
Your Name <you@example.com>
2022-04-04 06:31:46 +0000

    x

--- configs/porn_multi/porn_20220403.py
+++ configs/porn_multi/porn_20220403.py
@@ -150,13 +150,13 @@ data = dict(
         type=dataset_type,
         data_prefix='',
         classes=['性感_胸部', '色情_女胸', '色情_男下体', '色情_口交', '性感_内衣裤', '性感_男性胸部', '色情_裸露下体', '性感_腿部特写', '正常'],
-        ann_file='/home/zhou/projects/mmclassification/data/porn/20220403/eval.txt',
+        ann_file='/home/zhou/projects/mmclassification/data/porn/20220403/new_eval.txt',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
         data_prefix='',
         classes=['性感_胸部', '色情_女胸', '色情_男下体', '色情_口交', '性感_内衣裤', '性感_男性胸部', '色情_裸露下体', '性感_腿部特写', '正常'],
-        ann_file='/home/zhou/projects/mmclassification/data/porn/20220403/eval.txt',
+        ann_file='/home/zhou/projects/mmclassification/data/porn/20220403/new_eval.txt',
         pipeline=test_pipeline))
 load_from = 'https://download.openmmlab.com/mmclassification/v0/regnet/convert/RegNetX-4.0GF-ef8bb32c.pth'
 evaluation = dict(interval=5, metric=['mAP', 'CP', 'CR', 'CF1', 'OP', 'OR', 'OF1'],
