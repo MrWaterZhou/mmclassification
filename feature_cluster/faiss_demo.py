1f033efd1454f35896e10627a14e68e7802870c1
dragonchemist <785524367@qq.com>
2022-04-13 20:40:04 +0800

    x

--- feature_cluster/faiss_demo.py
+++ feature_cluster/faiss_demo.py
@@ -130,6 +130,7 @@ if __name__ == '__main__':
                         neighbors_result[label] = max(n[label], neighbors_result[label])
                 neighbors_result['image'] = source_data['image']
                 neighbors_result['neighbor'] = 0
+                neighbors_result['neighbor_images'] = [n['image'] for n in neighbors]
                 for label in labels:
                     neighbors_result['neighbor'] = max(neighbors_result['neighbor'], int(neighbors_result[label] != source_data[label]))
                 if neighbors_result['neighbor'] > 0:
