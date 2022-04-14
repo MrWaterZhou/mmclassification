3a56df171f3f478a2c6f7ed6f53904d12c5cd8a9
dragonchemist <785524367@qq.com>
2022-04-14 12:31:33 +0800

    x

--- feature_cluster/onnx_converter.py
+++ feature_cluster/onnx_converter.py
@@ -5,7 +5,11 @@ import sys
 model = onnx.load(sys.argv[1])
 intermediate_layer_value_info = helper.ValueInfoProto()
 intermediate_layer_value_info.name = '696'
-model.graph.output.pop()
+intermediate_layer_value_info.type.tensor_type.elem_type = 1
+intermediate_layer_value_info.type.tensor_type.shape.dim.append(model.graph.output[0].type.tensor_type.shape.dim[0])
+intermediate_layer_value_info.type.tensor_type.shape.dim.append(model.graph.output[0].type.tensor_type.shape.dim[1])
+intermediate_layer_value_info.type.tensor_type.shape.dim[1].dim_value = 1360
+
 model.graph.output.append(intermediate_layer_value_info)
-print(model.graph.output[0])
+print(model.graph.output[0], model.graph.output[1])
 onnx.save(model, sys.argv[2])
