65ad3b5c3e70d4a4a6cb8ee4a37398a0a7a3875a
dragonchemist <785524367@qq.com>
2022-04-15 18:22:46 +0800

    多中心测试

--- mmcls/models/heads/__init__.py
+++ mmcls/models/heads/__init__.py
@@ -4,12 +4,12 @@ from .conformer_head import ConformerHead
 from .deit_head import DeiTClsHead
 from .linear_head import LinearClsHead
 from .multi_label_head import MultiLabelClsHead
-from .multi_label_linear_head import MultiLabelLinearClsHead
+from .multi_label_linear_head import MultiLabelLinearClsHead, MultiCenterCls
 from .stacked_head import StackedLinearClsHead
 from .vision_transformer_head import VisionTransformerClsHead
 
 __all__ = [
     'ClsHead', 'LinearClsHead', 'StackedLinearClsHead', 'MultiLabelClsHead',
     'MultiLabelLinearClsHead', 'VisionTransformerClsHead', 'DeiTClsHead',
-    'ConformerHead'
+    'ConformerHead', 'MultiCenterCls'
 ]
