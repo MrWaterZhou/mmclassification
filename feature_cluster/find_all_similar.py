75db2567a7886a99db08d8b8a2d0dc552674d962
dragonchemist <785524367@qq.com>
2022-05-06 15:45:57 +0800

    x

--- feature_cluster/find_all_similar.py
+++ feature_cluster/find_all_similar.py
@@ -16,11 +16,14 @@ if __name__ == '__main__':
     for target_id, db_id in zip(target_ids, db_ids):
         image = target[target_id]['image']
         if image not in similar:
-            similar[image] = {'image': image, 'neighbor': []}
+            similar[image] = {'image': image, 'neighbor': [], 'neighbor_label': []}
         similar[image]['neighbor'].append(db[db_id]['image'])
+        for k in db[db_id]:
+            if k != 'image':
+                if db[db_id][k] == 1:
+                    similar[image]['neighbor_label'].append(k)
 
     with open(sys.argv[3], 'w') as f:
         for key in similar:
+            similar[key]['neighbor_label'] = list(set(similar[key]['neighbor_label']))
             f.write(json.dumps(similar[key]) + '\n')
-
-
